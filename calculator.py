e8092a7e960aefaf875b7ad85fe0719018d16991
ayushyadav635 <ayushyadav9988776@gmail.com>
2024-06-03 22:32:41 -0700

    new

---
+++ calculator.py
@@ -0,0 +1,31 @@
+#try:
+    #n1=int(input("enter value"))
+   # n2=int(input("enter value"))
+#try:
+  #  result= n1/n2
+ #   print(result)
+#except Exception as e:  
+   # print("insert only int values")
+#try:
+#    n1=int(input("enter value"))
+#    n2=int(input("enter value"))
+#    result=n1+n2
+#    print(result)
+#except Exception as e:
+#    print("insert only int values")
+
+#try:
+#    n1=int(input("enter value"))
+#    n2=int(input("enter value"))
+#    result= n1*n2
+#    print(result)
+#except Exception as e:  
+#    print("insert only int values")
+try:
+    n1=int(input("enter value"))
+    n2=int(input("enter value"))
+    result=n1-n2
+    print(result)
+except Exception as e:
+    print("insert only int values")
+
